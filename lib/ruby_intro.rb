70a2609cbf4c12b19036c58929f69251710e9eb4
kabriggs991 <kabriggs991@gmail.com>
2015-09-10 04:30:47 +0000

    Good stopping point

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,51 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  result = 0
+    arr.each do |item|
+        result = result + item
+    end
+    return result
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.size == 0
+   return arr[0] if arr.size == 1
+   
+   arr.sort!.reverse!
+   return arr[0] + arr[1]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.size == 0
+  return false if arr.size == 1
+
+   # [1,2,2,4], 4
+   # pull out all numbers greater than the input number
+   # 
+   
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s =~ /\A(?=[^aeiou])(?=[a-z])/i
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /\b[01]{4}\b/
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
